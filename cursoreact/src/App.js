d77e4751d27bd1dca38c55bfbaa1dcb2f2f2dcca
javimcl <javimcl1986@gmail.com>
2021-11-18 21:37:17 -0500

    Update App.js

--- cursoreact/src/App.js
+++ cursoreact/src/App.js
@@ -1,28 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
 
 function App() {
 
-  var nombre = "Javier"
-var saludo =  <p>
-Hola Mundo soy {nombre} <br/>
-Edit <code>src/App.js</code> and save to reload.
-</p>
-
+  const [linea1, setLinea1]=useState('');
+  const [linea2, setLinea2]=useState('');
+ const onChangeLinea1 = function(evento){
+   setLinea1(evento.target.value);
+ }
+ const onChangeLinea2 = function(evento){
+  setLinea2(evento.target.value);
+}
   return (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-       {saludo}
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <select>
+        <option value="casa">Casa en llamas</option>
+        <option value="futurama">Futurama</option>
+        <option value="history">History</option>
+        <option value="matriz">Matriz</option>
+        <option value="philosoraptor">Philosoraptor</option>
+        <option value="smart">Smart Guy</option>
+      </select><br/>
+      <input onChange={onChangeLinea1} type="text" placeholder="Linea 1"></input><br/>
+      <input onChange={onChangeLinea2} type="text"  placeholder="Linea 2"></input><br/>
+      <button>Exportar</button>
+      <div>
+        <span>{linea1}</span><br/>
+        <span>{linea2}</span>
+        <img src=""/>
+      </div>
+      
+
+     
     </div>
   );
 }
